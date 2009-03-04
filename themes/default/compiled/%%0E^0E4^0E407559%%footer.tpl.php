c4a689c7ae36e5772f77e4918c60dda64b4c538d
Gavin <halkeye@gmail.com>
2009-03-04 23:47:07 +0000

    removing compiled templates

--- themes/default/compiled/%%0E^0E4^0E407559%%footer.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2009-02-26 14:23:26
-         compiled from footer.tpl */ ?>
-</div> 
-<div id="footer"><p><a href="http://jigsaw.w3.org/css-validator/">CSS 2.1 Validated</a>, <a href="http://validator.w3.org/check?uri=referer">XHTML 1.0 Strict Validated</a>, <a href="mailto:stt@sfu.ca">Admin: stt@sfu.ca</a></p></div> 
- 
-</div> 
-</body> 
-</html> 
- 
