9bf82fd9b319f357091dadd842117c9256f8a60e
Gavin <halkeye@gmail.com>
2009-07-26 21:29:00 +0000

    cleaning up title var

--- system/application/views/layout.php
+++ system/application/views/layout.php
@@ -1,9 +1,14 @@
 <?php
 $this->load->helper('url');
+if (!isset($title))
+    $title = '';
+else
+    $title = ' :: ' . $title;
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
 <head> 
-<title>Electronic Convention Management (ECM) :: {$title}</title> 
+<title>Electronic Convention Management (ECM)<?php echo $title ?></title> 
 <link href="<?php echo base_url() ?>css/main.css" rel="stylesheet" type="text/css" /> 
 <script type="text/javascript" src="<?php echo base_url() ?>js/jquery-1.3.2.min.js"></script>
 </head> 
@@ -37,6 +42,7 @@ $this->load->helper('url');
     <br />
 </div> 
 <div id="footer"><p><a href="http://jigsaw.w3.org/css-validator/">CSS 2.1 Validated</a>, <a href="http://validator.w3.org/check?uri=referer">XHTML 1.0 Strict Validated</a>, <a href="mailto:stt@sfu.ca">Admin: stt@sfu.ca</a></p></div> 
+<!-- Page rendered in {elapsed_time} seconds -->
  
 </div> 
 </body> 
