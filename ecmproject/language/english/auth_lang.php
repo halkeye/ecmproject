fa87cacd78e6780032e590e83d13f1ae35400091
Gavin <halkeye@gmail.com>
2009-08-02 04:02:17 +0000

    Setup emails to be sent out upon registration You are not able to log in until your email is verified.
    Still needs work

---
+++ ecmproject/language/english/auth_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['auth_error_invalid_user_pass'] = "Invalid username or password.";
+$lang['auth_error_not_active']        = "Email has not been validated yet, please check your email for validation link.";
+$lang['auth_error_invalid_account']   = "Invalid account.";
+$lang['auth_error_validate_already_used'] = 'You have already used this one-time login link. It is not necessary to use this link to login anymore. You are already logged in.';
+$lang['auth_error_expired_validate_link'] = 'You have tried to use a validation link that has expired. Please request a new one.';
+
+/* End of file auth_lang.php */
+/* Location: ./application/language/english/auth_lang.php */
