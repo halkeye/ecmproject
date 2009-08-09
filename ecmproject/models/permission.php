3c89ff9b187c0fc9c338af4db1ff90bc683597de
Gavin <halkeye@gmail.com>
2009-08-09 09:15:07 +0000

    Added permission table support Added usergroup table support Added relationship data for account => usergroup Added Auth support for groups/permissions (stored in the session, added         has_group and has_persmission) Defaulted INNODB for table schemes
    (we really need to define a coding standard. function_name, or
     functionName, same with variables. Php actually makes camelcase easier
     as its case insensitive)

---
+++ ecmproject/models/permission.php
@@ -0,0 +1,53 @@
+<?php
+
+class Permission_Model extends orm 
+{
+    var $table_name = 'permissions';
+    #var $primary_key = 'permissions_id';
+
+    protected $table_columns = array (
+            'id'          => array ( 'type' => 'int',    'max' => 2147483647,    'unsigned' => true,    'sequenced' => true,  ),
+            'pkey'        => array ( 'type' => 'string', 'length' => '55',  ),
+            'description' => array ( 'type' => 'string', 'null' => true,  ),
+    );
+
+    public function unique_key($id = NULL) 
+    {
+        if (empty($id))
+            return parent::unique_key($id);
+
+        if (is_string($id))
+            return 'pkey';
+
+        return parent::unique_key($id);
+    }
+    
+    /**
+	 * Validates and optionally saves a new user record from an array.
+	 *
+	 * @param  array    values to check
+	 * @param  boolean  save[Optional] the record when validation succeeds
+	 * @return boolean
+	 */
+	public function validate(array & $array, $save = FALSE)
+	{
+		// Initialise the validation library and setup some rules
+		$array = Validation::factory($array);
+        // uses PHP trim() to remove whitespace from beginning and end of all fields before validation
+        $array->pre_filter('trim');
+
+        // Add Rules
+        $array->add_rules('pkey', 'required', 'unique');
+
+        // Email unique validation
+        //$array->add_callbacks('email', array($this, '_unique_name'));
+        //$array->add_rules('name', 'required', array($this, '_name_exists'));
+
+ 
+		return parent::validate($array, $save);
+	}
+
+}
+
+/* End of file permission.php */
+/* Location: ./application/models/permission.php */ 
