b2a283b1bedf971e7480f9f3146e43fa03cdad51
Gavin <halkeye@gmail.com>
2009-08-09 05:08:09 +0000

    authentication messages

---
+++ ecmproject/i18n/en_US/auth.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+$lang = array(
+        'expired_validate_link'     => 'blah blah blah, invalid',
+        'login_success'             => 'Login Success!',
+        /* Expired and/or already logged in and/or other stuff */
+        'bad_link'                  => 'Bad authentication Link',
+
+);
