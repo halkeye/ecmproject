3503459c00fc8452641ef7ebe0fbda64d0cf20e9
Stephen T <stt@sfu.ca>
2009-09-15 04:46:28 +0000

    Update to use html::anchor

--- modules/ecmproject/views/admin/index.php
+++ modules/ecmproject/views/admin/index.php
@@ -2,16 +2,16 @@
 <!-- CONTENT -->
 <table>
 	<tr>
-		<th class='header' width=50%><a href="admin/manageConventions">Manage conventions</a></th>
-		<th class='header' width=50%><a href="admin/managePasses">Manage passes</a></th>		
+		<th class='header' width=50%><?php echo html::anchor('admin/manageConventions', 'Manage Conventions') ?></th>
+		<th class='header' width=50%><?php echo html::anchor('admin/managePasses', 'Manage Passes') ?></th>		
 	</tr>
 	<tr>
 		<td>Add, modify or delete conventions in the system.</td>
 		<td>Add, modify or delete pass types associated with a convention - purchasable or otherwise</td>
 	</tr>
 	<tr>
-		<th class='header'><a href="admin/manageAccounts">Manage accounts</a></th>
-		<th class='header'><a href="admin/manageRegistrations">Manage registrations</a></th>	
+		<th class='header'><?php echo html::anchor('admin/manageAccounts', 'Manage Accounts') ?></th>
+		<th class='header'><?php echo html::anchor('admin/manageRegistrations', 'Manage Registrations') ?></th>	
 	</tr>
 	<tr>
 		<td>Add, modify, or delete user accounts in the system.</td>
