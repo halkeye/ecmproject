5371a3666ac94eef0e0d7d6cad0829b6d2aaa64d
Stephen T <stt@sfu.ca>
2009-09-15 04:11:47 +0000

    Admin views for use with Admin and more besides.

---
+++ modules/ecmproject/views/admin/index.php
@@ -0,0 +1,20 @@
+<br />
+<!-- CONTENT -->
+<table>
+	<tr>
+		<th class='header' width=50%><a href="admin/manageConventions">Manage conventions</a></th>
+		<th class='header' width=50%><a href="admin/managePasses">Manage passes</a></th>		
+	</tr>
+	<tr>
+		<td>Add, modify or delete conventions in the system.</td>
+		<td>Add, modify or delete pass types associated with a convention - purchasable or otherwise</td>
+	</tr>
+	<tr>
+		<th class='header'><a href="admin/manageAccounts">Manage accounts</a></th>
+		<th class='header'><a href="admin/manageRegistrations">Manage registrations</a></th>	
+	</tr>
+	<tr>
+		<td>Add, modify, or delete user accounts in the system.</td>
+		<td>Add, modify or delete convention registrations for user(s).</td>
+	</tr>
+</table>
---
+++ modules/ecmproject/views/admin/list.php
@@ -0,0 +1,23 @@
+<br />
+<!-- CONTENT: TODO: Renaming variables to something general. -->
+<table width='100%'>	
+	<tr>	
+		<th class='header' width=10%>ID</th>
+		<th class='header' width=30%>Email</th>
+		<th class='header' width=15%>Status</th>
+		<th class='header' width=20%>Last Login</th>
+		<th class='header' width=10%>Edit</th>
+		<th class='header' width=10%>Delete</th>
+	</tr>
+	<?php foreach ($entries as $rows): ?>
+	<tr>
+		<?php foreach ($rows as $column): ?>
+		<td><?php echo $column ?></td>
+		<?php endforeach; ?>
+		<!-- Also output actions as well -->
+		<?php foreach ($actions as $action): ?>
+		<td><?php echo $action ?></a></td>
+		<?php endforeach; ?>
+	</tr>
+	<?php endforeach; ?>
+</table>
