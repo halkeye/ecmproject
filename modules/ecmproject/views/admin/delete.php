b533d0720e346c62a41b53b5dc20bfb9b5097103
Stephen T <stt@sfu.ca>
2009-09-20 06:30:04 +0000

    Forgot to commit the other part (the view)

--- modules/ecmproject/views/admin/delete.php
+++ modules/ecmproject/views/admin/delete.php
@@ -2,8 +2,7 @@
 <!-- CONTENT: TODO: Renaming variables to something general. -->
 <p><strong>You are deleting the <?php echo $entityType ?>: <?php echo $entityName ?>.</strong> This action is <strong>NOT</strong> reversible. Do you wish to continue? </p>
 <br />
-<?php echo form::open('admin/deleteAccount'); ?>
-<input type="hidden" name="id" value=<?php echo $id ?>>
+<?php echo form::open("admin/deleteAccount/$id"); ?>
 <button name="No" value="No" type="submit" class='wide'>No</button>
 <button name="Yes" value="Yes" type="submit">Yes</button>
 <?php echo form::close(); ?>
