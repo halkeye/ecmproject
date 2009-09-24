10a64c38b82f6b071bf168cb05ef2aef52e76f19
Stephen T <stt@sfu.ca>
2009-09-24 05:37:29 +0000

    Fancy login. Note: it breaks if the side menu is enabled.

--- modules/ecmproject/views/user/loginOrRegister.php
+++ modules/ecmproject/views/user/loginOrRegister.php
@@ -1,90 +1,43 @@
 <br />
 <!-- CONTENT -->
+<div id='newform'>
 <table width='97%' class='login'>	
 	<tr>
-		<th class='header' width='50%'>Create Log-in ID</th>
-		<th class='header' width='50%'>Log-in with Existing ID</th>
-	</tr>
-	<tr>
-		<td>
-			<p>Please enter your e-mail address and a password of your choice and click <strong>Continue</strong>.  
-			The e-mail address will serve as your login ID when you return to this site.</p>
+		<td width='50%'>
 			<?php echo form::open('/user/register'); ?>
-				<fieldset>
-					<table cellspacing="0" cellpadding="2" border="0">
-						<tr><td colspan="3">&nbsp;</td></tr>
-						<tr>
-							<td class="fieldlabel"><DIV>E-mail Address:</DIV></TD>
-							<td><span class="required">*</span></td>
-							<td><input name="email" type="text"  style="width:156px;" /></td>
-						</tr>
-						<tr>
-							<td class="fieldlabel"><div>Password:</div></td>
-							<td><span class="required">*</span></td>
-							<td><input name="password" type="password" style="width:156px;" /></td>
-						</tr>
-						<tr>
-							<td class="fieldlabel"><div>Re-type Password:</div></td>
-							<td><span class="required">*</span></td>
-							<td><input name="confirm_password" type="password" style="width:156px;" /></td>
-						</tr>
-						<tr>
-							<td class="fieldlabel"><div></div></td>
-							<td></td>
-							<td><input type="submit" value="Continue" /></td>
-						</tr>
-					</table>
-				</fieldset>
-				<?php echo form::close(); ?>
-		</td>
-		<td>
-		<p>Please enter your e-mail address and password and click <strong>Continue</strong>.</p>
-		<?php echo form::open('/user/login'); ?>
-			<fieldset>					
-				<table cellspacing="0" cellpadding="2" border="0">
-					<tr><td colspan="3">&nbsp;</td></tr>
-					<tr>
-						<td class="fieldlabel"><DIV>E-mail Address:</DIV></TD>
-						<td><span class="required">*</span></td>
-						<td><input name="email" type="text"  style="width:156px;" /></td>
-					</tr>
-					<tr>
-						<td class="fieldlabel"><div>Password:</div></td>
-						<td><span class="required">*</span></td>
-						<td><input name="password" type="password" style="width:156px;" /></td>
-					</tr>
-					<tr>
-						<td class="fieldlabel"><div></div></td>
-						<td></td>
-						<td><input type="submit" value="Continue" /></td>
-					</tr>
-				</table>
-				<div id="_ctl9__ctl28_LoginControl1_DivForgotPassword" ms_positioning="FlowLayout">
-					<strong><span>Forgot your password?</span>:</strong> 
-					<?php echo html::anchor("/user/lostPassword", "Click here"); ?> to have your password emailed to you.
-				</div>
+			<h1>Create Log-in ID</h1>
+			<p>Please enter your e-mail address and a password of your choice and click Continue. The e-mail address will serve as your login ID when you return to this site.</p>
+			<fieldset class='fixed'>			
+				<label for="email">Email Address <span class="small">Add a valid email address</span></label>
+				<input name="email" type="text" />
+				<label for="password">Password <span class="small">Min. size 6 characters</span></label>
+				<input name="password" type="password" />
+				<label for="confirm_password">Re-type Password <span class="small">Type in the same characters again.</span></label>
+				<input name="confirm_password" type="password" />
+			</fieldset>
+			<fieldset class='right'>
+				<button type="submit">Continue</button>
 			</fieldset>
 			<?php echo form::close(); ?>
 		</td>
+		<td width='50%'>
+			<?php echo form::open('/user/login'); ?>
+			<h1>Log-in with Existing ID</h1>
+			<p>Please enter your e-mail address and password and click <strong>Continue</strong>.</p>
+		
+			<fieldset class='fixed'>		
+				<label for="email">Email Address <span class="small">Add a valid email address</span></label>
+				<input name="email" type="text" />
+				<label for="password">Password <span class="small">Min. size 6 characters</span></label>
+				<input name="password" type="password" />	
+				<label><?php echo html::anchor("/user/lostPassword", "Forgot your password?"); ?></label>	
+			</fieldset>			
+			<fieldset class='right'>				
+				<button type="submit">Continue</button>
+			</fieldset>							
+			<?php echo form::close(); ?>
+			
+		</td>
 	</tr>
 </table>
-
-<table cellspacing="0" cellpadding="0" width="97%" border="0">
-    <tr>
-        <td></td>
-        <td valign="top">
-            <table id="_ctl9__ctl28_Table1" cellspacing="1" cellpadding="1" width="100%" border="0">
-                <tr>
-                    <td colspan="2" align="left"></td>
-                </tr>
-                <tr>
-                    <td align="left" width="50%">                       
-                    </td>
-                    <td width="50%">
-                        
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+</div>
