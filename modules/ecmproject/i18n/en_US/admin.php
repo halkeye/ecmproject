292e4e8b93d3120ad8e92a08035849b3478f5896
Stephen T <stt@sfu.ca>
2009-09-29 04:24:59 +0000

    Language file (en_US) for Admin.

---
+++ modules/ecmproject/i18n/en_US/admin.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+$lang = array(
+        'admin_area' => 'Administration Area',
+        'admin_area_desc'       => 'Manage conventions, passes, permissions and other details',
+        'admin_accounts'         => 'Manage Accounts',
+        'admin_conventions'              => 'Manage Conventions',
+        'admin_passes'         => 'Manage Passes',
+        'admin_registrations'               => 'Manage Registrations',       
+
+        'edit_account'            => 'Edit this account',
+        'delete_account'                => 'Delete this account',
+        'add_account'                => 'Add account',
+		
+        'account_not_exist'            => 'The account specified no longer exists.',
+);
