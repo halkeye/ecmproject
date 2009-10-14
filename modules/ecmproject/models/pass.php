f624e71451d1cb5ab2be5c5470612d59b477df2c
Stephen T <stt@sfu.ca>
2009-10-14 05:12:30 +0000

    Date format validation via callbacks in Pass model.

--- modules/ecmproject/models/pass.php
+++ modules/ecmproject/models/pass.php
@@ -34,10 +34,15 @@ class Pass_Model extends orm
 		$array = Validation::factory($array);
         $array->pre_filter('trim');
 	
+		$array->add_rules('convention_id', 'required');
 		$array->add_rules('name', 'required');
 		$array->add_rules('price', 'required'); //Also set
-		$array->add_rules('isPurchasable', 'required'); //Will be set on form. 	
-
+		$array->add_callbacks('startDate', array($this, '__validateISODate')); //Non-set start date will be set to today
+		$array->add_callbacks('endDate', array($this, '__validateISODate')); //Non-set end date will be set to convention end.
+		
+		if (!isset($isPurchasable))
+			$array->isPurchasable = 0;
+		
 		if (isset($array->minAge) && !empty($array->minAge))
 			$array->add_rules('minAge', 'required', 'numeric');
 			
@@ -46,10 +51,28 @@ class Pass_Model extends orm
 				
 		return parent::validate($array, $save);
 	}
+	
     public function __toString()
     {
         return $this->name . ' - ' . sprintf('$%01.2F', $this->price);
     }
+	
+	public function __validateISODate(Validation $array, $field)
+	{
+		$regex = '/(\d{2})-(\d{2})-(\d{4})/';		
+		
+		// PHP regex matching seems so damn picky relative to Java. Can't even match an escaped backslash -_-;
+		$string = str_replace("/", "-", $array->$field);
+		
+		//If the field is not set or empty
+		if (!isset($array->$field) || empty($array->$field))
+		{
+			//Do nothing. 
+		}		
+		else if ( ! preg_match($regex, $string) ) {
+			$array->add_error($field, 'invalid_date');
+		}
+	}
 }
 
 /* End of file pass.php */
