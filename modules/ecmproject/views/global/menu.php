c856ab258bda6304c415c370a0695690c542916b
Gavin <halkeye@gmail.com>
2009-10-16 21:20:14 +0000

    make menu items use the 'currentMenuChoice' class if we are at that url

--- modules/ecmproject/views/global/menu.php
+++ modules/ecmproject/views/global/menu.php
@@ -1,8 +1,16 @@
 <?php foreach ($menu as $m): ?>
 <li>
 <?php
-if (isset($m['seperator'])) { echo '-----'; }
-else { echo html::anchor($m['url'], $m['title']); }
+if (isset($m['seperator'])) 
+{ 
+    echo '-----'; 
+}
+else
+{
+    $attributes = array();
+    if ($m['url'] == url::current()) { $attributes['class'] = 'currentMenuChoice'; }
+    echo html::anchor($m['url'], $m['title'], $attributes); 
+}
 ?>
 </li>
 <?php endforeach; ?>
