2e145698c84523e20d5cbcaf298789f0664ad072
Gavin <halkeye@gmail.com>
2009-10-17 21:11:22 +0000

    forgot to add the lang file in last commit

---
+++ modules/ecmproject/i18n/en_US/convention.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+$lang = array(
+        'checkout_with_paypal'  => 'Pay with Paypal',
+        'checkout_with_other'   => 'Pay via Mail/In-Person',
+        'checkout_heading'      => 'Shopping Cart',
+        'checkout_subheading'   => "The badges that you've signed up for so far",
+        'checkout_header'       => 'Listed here are all the badges that you wish to register for. Bear in mind that your registration is <strong>not yet complete</strong> until you checkout by selecting <strong>Pay with Paypal</strong> or <strong>Pay via Main/In-Person</strong>. If you wish to add more badges, select the "Add Registration" menu link on the left hand side.',
+);
