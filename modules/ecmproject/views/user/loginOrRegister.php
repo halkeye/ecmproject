7adf966bbcf3c70ca527f931c8929b1cf459a133
Stephen T <stt@sfu.ca>
2009-10-17 20:55:06 +0000

    Front page fix with new styling.

--- modules/ecmproject/views/user/loginOrRegister.php
+++ modules/ecmproject/views/user/loginOrRegister.php
@@ -1,12 +1,11 @@
-<br />
 <!-- CONTENT -->
-<div id='newform'>
-<table width='97%' class='login'>	
+<div id='form'>
+<table width='100%' class='login'>	
 	<tr>
 		<td width='50%'>
 			<?php echo form::open('/user/register'); ?>
 			<h1>Create Log-in ID</h1>
-			<p>Please enter your e-mail address and a password of your choice and click Continue. The e-mail address will serve as your login ID when you return to this site.</p>
+			<p class='fixed'>Please enter your e-mail address and a password of your choice and click Continue. The e-mail address will serve as your login ID.</p>
 			<fieldset class='fixed'>			
 				<label for="email">Email Address <span class="small">Add a valid email address</span></label>
 				<input name="email" type="text" />
@@ -15,7 +14,7 @@
 				<label for="confirm_password">Re-type Password <span class="small">Type in the same characters again.</span></label>
 				<input name="confirm_password" type="password" />
 			</fieldset>
-			<fieldset class='right'>
+			<fieldset class='left'>
 				<button type="submit">Continue</button>
 			</fieldset>
 			<?php echo form::close(); ?>
@@ -23,16 +22,17 @@
 		<td width='50%'>
 			<?php echo form::open('/user/login'); ?>
 			<h1>Log-in with Existing ID</h1>
-			<p>Please enter your e-mail address and password and click <strong>Continue</strong>.</p>
+			<p class='fixed'>Please enter your e-mail address and password and click <strong>Continue</strong>.</p>
 		
 			<fieldset class='fixed'>		
 				<label for="email">Email Address <span class="small">Add a valid email address</span></label>
 				<input name="email" type="text" />
 				<label for="password">Password <span class="small">Min. size 6 characters</span></label>
 				<input name="password" type="password" />	
-				<p><?php echo html::anchor("/user/lostPassword", "Forgot your password?"); ?></p>	
+				
+				<?php echo html::anchor("/user/lostPassword", "Forgot your password?"); ?>
 			</fieldset>			
-			<fieldset class='right'>				
+			<fieldset class='left'>				
 				<button type="submit">Continue</button>
 			</fieldset>							
 			<?php echo form::close(); ?>
