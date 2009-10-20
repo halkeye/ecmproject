2f2be004e4c8e4b6d293ad6f78a5335e4e245774
Gavin <halkeye@gmail.com>
2009-10-20 04:19:20 +0000

    removing elapsed time from template file

--- modules/ecmproject/views/template.php
+++ modules/ecmproject/views/template.php
@@ -45,7 +45,6 @@
     <br />
 </div> 
 <div id="footer"><p><a href="http://jigsaw.w3.org/css-validator/">CSS 2.1 Validated</a>, <a href="http://validator.w3.org/check?uri=referer">XHTML 1.0 Strict Validated</a>, <a href="mailto:stt@sfu.ca">Admin: stt@sfu.ca</a></p></div> 
-<!-- Page rendered in {elapsed_time} seconds -->
  
 </div> 
 
