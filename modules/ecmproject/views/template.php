e2f8c333e926ce0c3fec9b4d8e8c397e4fc3fd86
Stephen T <stt@sfu.ca>
2009-11-01 18:13:27 +0000

    The bottom footer also aligns depending on whether or not there's a login box.

--- modules/ecmproject/views/template.php
+++ modules/ecmproject/views/template.php
@@ -44,7 +44,7 @@
     <!-- content end -->
     <br />
 </div> 
-<div id="footer"><p><a href="http://jigsaw.w3.org/css-validator/">CSS 2.1 Validated</a>, <a href="http://validator.w3.org/check?uri=referer">XHTML 1.0 Strict Validated</a>, <a href="mailto:stt@sfu.ca">Admin: stt@sfu.ca</a></p></div> 
+<div id="footer" <?php if ($isLoggedIn || $menu) { echo " class='contentMenu'"; } else { echo " class='contentNoMenu'"; } ?>><p><a href="http://jigsaw.w3.org/css-validator/">CSS 2.1 Validated</a>, <a href="http://validator.w3.org/check?uri=referer">XHTML 1.0 Strict Validated</a>, <a href="mailto:stt@sfu.ca">Admin: stt@sfu.ca</a></p></div> 
  
 </div> 
 
