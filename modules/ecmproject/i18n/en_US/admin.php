b02f59013a2400327350556ef00e5ac050d5a014
Stephen T <stt@sfu.ca>
2009-11-01 18:08:36 +0000

    Updated lang file for admin forms.

--- modules/ecmproject/i18n/en_US/admin.php
+++ modules/ecmproject/i18n/en_US/admin.php
@@ -1,5 +1,6 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 $lang = array(
+		/* Informational messages go here */
         'admin_area' => 'Administration Area',
         'admin_area_desc'       => 'Manage conventions, passes, permissions and other details',
         'admin_accounts'         => 'Manage Accounts',
@@ -12,4 +13,24 @@ $lang = array(
         'add_account'                => 'Add account',
 		
         'account_not_exist'            => 'The account specified no longer exists.',
+		
+		/* Headers, subheaders, titles */
+		
+		/* Form fields and informational messages. */
+		'pass_field_name' => 'Pass Name',
+		'pass_field_price' => 'Price',
+		'pass_field_convention_id' => 'Convention',		
+		'pass_field_startDate' => 'Start Date',
+		'pass_field_endDate' => 'End Date',
+		'pass_field_minAge' => 'Minimum Age',
+		'pass_field_maxAge' => 'Maximum Age',
+		'pass_field_isPurchasable' => 'Purchasable?',		
+		'pass_field_email' => 'Email',
+		'pass_field_password' => 'Password',
+		'pass_field_confirm_password' => 'Confirm Password',
+		'pass_field_status' => 'Validation Status',
+		'pass_field_conv_name' => 'Convention Name', 
+		'pass_field_conv_start_date' => 'Start Date', 
+		'pass_field_conv_end_date' => 'End Date', 
+		'pass_field_conv_location' => 'Location'
 );
