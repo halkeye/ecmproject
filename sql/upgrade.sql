bd4379a0bb0ad328983ca21a4e72bdc8e07cb4af
Gavin <halkeye@gmail.com>
2009-11-30 00:06:14 +0000

    updates to get db up to latest schema

---
+++ sql/upgrade.sql
@@ -0,0 +1,3 @@
+alter table registrations change phone phone VARCHAR(25) NOT NULL;
+alter table registrations change cell cell VARCHAR(25) NOT NULL;
+alter table registrations change ephone ephone VARCHAR(25) NOT NULL;
