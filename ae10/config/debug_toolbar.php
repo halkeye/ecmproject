d6bd93ad8f0c0affd0a550f588623fb0a5b2ab2a
Gavin <halkeye@gmail.com>
2009-12-10 05:42:55 +0000

    theme of ae10

---
+++ ae10/config/debug_toolbar.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$config['auto_render']     = TRUE;
+$config['icon_path']       = 'modules/debug_toolbar/images';
+$config['firephp_enabled'] = FALSE;
+$config['secret_key']      = "abc123";
