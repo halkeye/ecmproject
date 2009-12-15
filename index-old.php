478105964416bb325e371a3cfe3d936cd478a19d
Gavin <halkeye@gmail.com>
2009-12-15 05:37:53 +0000

    renaming to default ae10-uchi theme

--- index-old.php
+++ index-old.php
@@ -22,7 +22,7 @@ define('IN_PRODUCTION', TRUE);
  *
  * This path can be absolute or relative to this file.
  */
-$kohana_application = 'ae10-uchi';
+$kohana_application = 'application';
 
 /**
  * Kohana modules directory. This directory should contain all the modules used
--- index.php
+++ index.php
@@ -22,7 +22,7 @@ define('IN_PRODUCTION', TRUE);
  *
  * This path can be absolute or relative to this file.
  */
-$kohana_application = 'application';
+$kohana_application = 'ae10-uchi';
 
 /**
  * Kohana modules directory. This directory should contain all the modules used
