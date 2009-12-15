9d076939e0e22d24701e59c7a3e95405269060df
Stephen T <stt@sfu.ca>
2009-12-15 05:32:52 +0000

    Update ae10-uchi to match mockup that Greg sent out today. Notes:
    * Stripped the shadow from the banner image as implementing a shadow for the rest of the container is more work than I want to spare right now.
    * For future maintenance, the changing of two lines in main2.css (or whatever it will be named in final) is all that needs to be modified.

--- ae10-uchi/views/template.php
+++ ae10-uchi/views/template.php
@@ -1,115 +1,53 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head> 
+<title>Electronic Convention Management (ECM)<?php echo $title ? "::$title" : "" ?></title> 
+<?php echo html::stylesheet(array('css/main2.css')); ?>
+<?php echo html::script(array('js/jquery-1.3.2.min.js')); ?>
+</head> 
+ 
+<body> 
+ 
+<!-- Content container beings here --> 
+<div id="container"> 
+    <!-- Header --> 
+    <div id="header"></div> 
+ 
+    <!-- Left Sidebar. --> 
+    <?php if ($isLoggedIn || $menu): ?>
+    <div id="sidebar">
+        <ul> 
+            <li class="title"><a href="<?php echo url::base(); ?>">Welcome</a></li>
+            <?php if ($isLoggedIn): ?>
+            <li><b><?php echo htmlentities($account->email) ?></b></li>
+            <li>&nbsp;</li>
+            <?php endif; ?>
 
-	<title>Anime Evolution 2010 &raquo; <?php echo $title ? "$title" : "" ?></title>
-    <?php echo html::stylesheet(array('css/ae10-ecm2.css')); ?>
+            <?php if($menu) { echo View::Factory('global/menu', array('menu'=>$menu)); } ?>
+        </ul>
+    </div> 
+    <?php endif; ?>
+ 
+    <!-- Content Pane (Right side) --> 
+    <div id="content"<?php if ($isLoggedIn || $menu) { echo " class='contentMenu'"; } else { echo " class='contentNoMenu'"; }?>>
+    <?php if ($heading) echo "<h2>$heading</h2>"; ?>
+    <?php if ($subheading) echo "<h3>$subheading</h3>"; ?>
+    
+    <?php foreach ($messages as $msg) { echo '<p class="msg">'.$msg.'</p>'; } ?>
+    <?php foreach ($errors as $err) { echo '<p class="errormsg">'.$err.'</p>'; } ?>
+    <?php if ($messages || $errors) { echo '<br />'; } ?>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-	<meta name="generator" content="WordPress 2.7.1" /> <!-- leave this for stats please -->
+    <!-- content start -->
+    <?php print $content ?>
+    <?php print $profiler ?>
+    <!-- content end -->
+    <br />
+</div> 
+<div id="footer" <?php if ($isLoggedIn || $menu) { echo " class='contentMenu'"; } else { echo " class='contentNoMenu'"; } ?>><p><a href="http://jigsaw.w3.org/css-validator/">CSS 2.1 Validated</a>, <a href="http://validator.w3.org/check?uri=referer">XHTML 1.0 Strict Validated</a>, <a href="mailto:stt@sfu.ca">Admin: stt@sfu.ca</a></p></div> 
+ 
+</div> 
 
-	<link rel="stylesheet" href="http://www.animeevolution.com/main/wp-content/themes/ae2010/style.css" type="text/css" media="screen" />
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="http://www.animeevolution.com/?feed=rss2" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="http://www.animeevolution.com/?feed=rss" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="http://www.animeevolution.com/?feed=atom" />
-	<link rel="pingback" href="http://www.animeevolution.com/main/xmlrpc.php" />
+</body> 
+</html> 
 
-		<link rel='archives' title='November 2009' href='http://www.animeevolution.com/?m=200911' />
-	<link rel='archives' title='September 2009' href='http://www.animeevolution.com/?m=200909' />
-	<link rel='archives' title='July 2009' href='http://www.animeevolution.com/?m=200907' />
-	<link rel='archives' title='June 2009' href='http://www.animeevolution.com/?m=200906' />
-	<link rel='archives' title='May 2009' href='http://www.animeevolution.com/?m=200905' />
-	<link rel='archives' title='April 2009' href='http://www.animeevolution.com/?m=200904' />
-	<link rel='archives' title='March 2009' href='http://www.animeevolution.com/?m=200903' />
-	<link rel='archives' title='February 2009' href='http://www.animeevolution.com/?m=200902' />
-	<link rel='archives' title='January 2009' href='http://www.animeevolution.com/?m=200901' />
-	<link rel='archives' title='October 2008' href='http://www.animeevolution.com/?m=200810' />
-		<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.animeevolution.com/main/xmlrpc.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.animeevolution.com/main/wp-includes/wlwmanifest.xml" /> 
-
-<!-- Multi-level Navigation Plugin v2.1.7 by PixoPoint Web Development ... http://pixopoint.com/multi-level-navigation/ -->
-<link rel="stylesheet" type="text/css" href="http://www.animeevolution.com/main/wp-content/plugins/multi-level-navigation-plugin/style.php" />
-<script type='text/javascript' src='http://www.animeevolution.com/main/wp-content/plugins/countdown-timer/fergcorp_countdownTimer_java.js?ver=2.3.5'></script>
-<script type='text/javascript' src='http://www.animeevolution.com/main/wp-content/plugins/countdown-timer/webtoolkit.sprintf.js?ver=2.3.5'></script>
-<script type='text/javascript' src='http://www.animeevolution.com/main/wp-includes/js/jquery/jquery.js?ver=1.2.6'></script>
-<script type='text/javascript' src='http://www.animeevolution.com/main/wp-content/plugins/multi-level-navigation-plugin/scripts/hoverIntent.js.php?ver=r5'></script>
-<script type='text/javascript' src='http://www.animeevolution.com/main/wp-content/plugins/multi-level-navigation-plugin/scripts/superfish.js?ver=1.4.8'></script>
-<script type='text/javascript' src='http://www.animeevolution.com/main/wp-content/plugins/multi-level-navigation-plugin/scripts/superfish_settings.js.php?ver=1.0'></script>
-<meta name="generator" content="WordPress 2.7.1" />
-
-<script type="text/javascript" src="http://www.animeevolution.com/main/wp-content/plugins/flash-video-player/swfobject.js"></script>
-
-	<link rel="stylesheet" type="text/css" href="http://www.animeevolution.com/main/wp-content/themes/ae2010/tabcontent.css" />
-
-	<!-- Flash menu commands -->
-	<script type="text/javascript">
-	function menuOn()
-	{
-		document.getElementById("menu").style.visibility = 'visible';
-		document.getElementById("image").style.visibility = 'hidden';
-	}
-	function menuOff()
-	{
-		document.getElementById("menu").style.visibility = 'hidden';
-		document.getElementById("image").style.visibility = 'visible';
-	}
-	</script>
-
-	<script type="text/javascript" src="http://www.animeevolution.com/main/wp-content/themes/ae2010/tabcontent.js">
-	
-
-	/***********************************************
-	* Tab Content script v2.2- � Dynamic Drive DHTML code library (www.dynamicdrive.com)
-	* This notice MUST stay intact for legal use
-	* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
-	***********************************************/
-
-	</script>	
-</head>
-<body onLoad="checkJavaScriptStatus()" background="http://www.animeevolution.com/main/wp-content/themes/ae2010/images/bg.jpg" style="padding:0px; margin:0px;">
-<div style="width:971px; margin: 0 auto;">
-	<div id="image" onmouseover="menuOn();" style="position:absolute; top:176px;">
-		<img src="http://www.animeevolution.com/main/wp-content/themes/ae2010/images/ae_menu.jpg" />
-	</div>
-	<div id="menu" onmouseout="menuOff();" style="position:absolute; top:176px;">
-		<embed src="http://www.animeevolution.com/main/wp-content/themes/ae2010/ae_menu.swf" type="application/x-shockwave-flash" name="ae_menu" width="971" height="200" align="middle" quality="high" allowScriptAccess="always" wmode="transparent" pluginspage="http://www.macromedia.com/go/getflashplayer" />
-	</div>
-</div>
-
-<table border="0" cellspacing="0" cellpadding="0" align="center" width="971">
-	<tr>
-		<td colspan="2"><img src="http://www.animeevolution.com/main/wp-content/themes/ae2010/images/top.png" /></td>
-	</tr>
-	<tr>
-		<td colspan="2" width="971" height="24"></td>
-	</tr>
-	<tr>
-		<td width="956" valign="top" bgcolor="#B3D8EF">
-		<table border="0" cellspacing="0" cellpadding="10px" width="100%">
-			<tr>
-				<td width="100%" valign="top">
-					<!-- content starts -->
-					<div id="content" class="ecmsbox">
-
-						<?php if ($heading) echo "<h2>$heading</h2>"; ?>
-						<?php if ($subheading) echo "<h3>$subheading</h3>"; ?>
-						
-						<?php foreach ($messages as $msg) { echo '<p class="msg">'.$msg.'</p>'; } ?>
-						<?php foreach ($errors as $err) { echo '<p class="errormsg">'.$err.'</p>'; } ?>
-						<?php if ($messages || $errors) { echo '<br />'; } ?>
-
-						<!-- content start -->
-						<?php print $content ?>
-						<?php print $profiler ?>
-						<!-- content end -->
-						<br />
-					</div> <!-- content ends here -->
-				</td>		
-			</tr>
-		</table>
-		</td>
-		<td width ="15" style="background-image:url('http://www.animeevolution.com/main/wp-content/themes/ae2010/images/frame_shadow.png'); background-repeat:repeat-y;"></td>
-	</tr>
-</table>
-</body>
-</html>
