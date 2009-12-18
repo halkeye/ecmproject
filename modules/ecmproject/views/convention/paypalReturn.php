02f24e79d832fbf61e444641496659821dbd0857
Stephen T <stt@sfu.ca>
2009-12-18 06:02:43 +0000

    Add some emphasis!

--- modules/ecmproject/views/convention/paypalReturn.php
+++ modules/ecmproject/views/convention/paypalReturn.php
@@ -1,3 +1,3 @@
-<div>
- You will get an email in your mailbox when payment is confirmed.
+<div id="list">
+ <p><strong>THANK YOU!</strong> You will get an email in your mailbox when payment is confirmed.</p>
 </div>
