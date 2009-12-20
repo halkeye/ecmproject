a7a3cc88834e83165a3cf65f86aae09bf7a377a6
Stephen T <stt@sfu.ca>
2009-12-20 20:45:02 +0000

    Add a header.

--- modules/ecmproject/views/user/register_confirmation.php
+++ modules/ecmproject/views/user/register_confirmation.php
@@ -1,3 +1,5 @@
+<h1>Anime Evolution Registration Confirmation</h1>
+
 <p><strong>Please read the entire email lest you miss something important!</strong></p>
 
 <p>As of <strong><?php echo date("M j, Y H:i", time()); ?></strong>, your registration to <strong><?php echo $conv->name; ?></strong> has been confirmed to have been paid in full.
