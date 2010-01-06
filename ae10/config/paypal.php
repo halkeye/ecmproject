b38d68b4280e122606f5b4b1940a7a5451c7ce80
Gavin <halkeye@gmail.com>
2010-01-06 06:41:04 +0000

    copy/move configs around, remove index_dev

---
+++ ae10/config/paypal.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * @FIXME: Describe config variables 
+ */
+#$config['url'] = 'https://www.paypal.com/cgi-bin/webscr';
+$config['url'] = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
+$config['business'] = 'AE10_1249882783_biz@gavinmogan.com';
+$config['image_dir'] = 'modules/paypal/images/';
