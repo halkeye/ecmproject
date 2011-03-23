0fae7b91c0bdd2f8d8c8069ec1287e389cdf9661
Gavin <halkeye@gmail.com>
2011-03-22 22:34:33 -0700

    more or less starting to work

---
+++ application/classes/base/maintemplate.php
@@ -0,0 +1,175 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+
+class Base_MainTemplate extends Controller_Template
+{
+    protected $auth = null;
+	// Session instance
+	protected $session;
+    
+    public $template = 'mainTemplate';
+
+    function before()
+    {
+        $ret = parent::before();
+        /* Session Instance */
+        $this->session = Session::instance();
+
+        /* Auth to handle */
+        $this->auth = Auth::instance();
+
+        /* Default Content */
+        $this->template->content = "";
+        /* Default Title */
+        $this->template->title = "";
+        /* Menu options */
+        $this->template->menu    = array();
+
+        /* Messages to show */
+        $this->template->messages = array();
+
+        /* Errors to show */
+        $this->template->errors = array();
+
+        $this->template->profiler = '';
+        
+        if ($this->auth->is_logged_in())
+        {
+            $this->isVerifiedAccount = TRUE;
+            $user = $this->auth->get_user();
+            if ($user->status != Account_Model::ACCOUNT_STATUS_VERIFIED)
+            {
+                /* check the database not the cached version for determining if they are cached or not
+                 * Maybe we can get away with not doing this 
+                 */
+                $user = ORM::factory('Account', array('id'=>$user->id));
+                if ($user->status != Account_Model::ACCOUNT_STATUS_VERIFIED)
+                {
+                    $this->isVerifiedAccount = FALSE;
+                    $this->template->errors[] = Kohana::lang('ecmproject.not_validated');
+                    $this->addMenuItem(array('title'=>'Verify Account', 'url'=>'/user/verifyMenu'));
+                }
+                else
+                {
+                    /* Store the updated account if we've been verified */
+                    $this->auth->storeAccount($user);
+                }
+            }
+            $this->addMenuItem(array('title'=>'My Account', 'url'=>'user'));
+
+            if ($this->auth->hasPermission('admin'))
+            {
+                $this->addMenuItem(array('title'=>'Administration', 'url'=>'admin'));
+                $this->addMenuItem(array('seperator'=>1));
+            }
+        }
+        return $ret;
+    }
+
+    public function after()
+    {
+
+        if (!isset($this->template->heading))
+            $this->template->heading = ucfirst($this->request->controller());
+        if (!isset($this->template->subheading))
+            $this->template->subheading = ucfirst($this->request->action());
+
+        $session_messages = $this->session->get_once('messages');
+        if ($session_messages) 
+            $this->template->messages = array_merge($session_messages, $this->template->messages);
+        $session_errors = $this->session->get_once('errors');
+        if ($session_errors) 
+            $this->template->errors = array_merge($session_errors, $this->template->errors);
+
+        /* Are you logged in? */
+        $this->template->set_global('isLoggedIn', $this->auth->is_logged_in());
+        $this->template->set_global('isVerifiedAccount', isset($this->isVerifiedAccount) ? $this->isVerifiedAccount : FALSE);
+        /* store the user */
+        $this->template->set_global('account', $this->auth->getAccount());
+        
+        if ($this->template->isLoggedIn)
+        {
+            $this->addMenuItem(array('url'=>'convention/checkout', 'title'=>'Checkout'));
+            $this->addMenuItem(array('seperator'=>1));
+
+
+            $this->addMenuItem(array('url'=>'user/logout', 'title'=>'Logout'));
+        }
+        return parent::after();
+    }
+
+    protected function addMessage($message)
+    {
+        $messages = $this->session->get('messages') or array();
+        $messages[] = $message;
+        $this->session->set_flash('messages',  $messages);
+    }
+    
+    protected function addError($error)
+    {
+        $errors = $this->session->get('errors') or array();
+        $errors[] = $error;
+        $this->session->set_flash('errors',  $errors);
+    }
+
+    /**
+     * Redirect a user to a location, unless a session variable is set
+     * @param string $where Where to redirect the user if nothing else is set
+     */
+    function _redirect($where = '')
+    {
+        $location = $this->session->get_once('redirected_from');
+        if (!$location) $location = $where;
+        url::redirect($location);
+        return;
+    }
+
+    protected function requireLogin()
+    {
+        if (!$this->auth->is_logged_in()) 
+        {
+            $this->session->set('redirected_from', url::current());
+            url::redirect('/user/loginOrRegister');
+            return;
+        }
+    }
+
+    protected function requireVerified()
+    {
+        if ($this->auth->is_logged_in() && !$this->isVerifiedAccount ) 
+        {
+            /* You can't go any furthur until email address is verified. */
+            $this->addError(Kohana::lang('auth.not_validated')); 
+            $this->session->set('redirected_from', url::current());
+            url::redirect('/user/verifyMenu');
+            return;
+        }
+    }
+
+    protected function requirePermission($permission) 
+    {
+        $this->requireLogin();
+        if (!$this->auth->hasPermission($permission))
+        {
+            /* Kohana has no forward, so redirect */
+            url::redirect('/user/accessDenied');
+        }
+    }
+    protected function requireGroup($group) {}
+
+    protected function addMenuItem($item)
+    {
+        $items = $this->view->menu;
+        $items[] = $item;
+        $this->view->menu = $items;
+    }
+
+    public function accessDenied()
+    {
+        $this->view->title = Kohana::lang('auth.accessDenied_title');
+        $this->view->heading = Kohana::lang('auth.accessDenied_heading');
+        $this->view->subheading = Kohana::lang('auth.accessDenied_subheading');
+        $this->view->content = new View('global/accessDenied');
+    }
+
+}
--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -1,23 +1,10 @@
 <?php defined('SYSPATH') OR die('No Direct Script Access');
  
-Class Controller_Welcome extends Controller_Template
+Class Controller_Welcome extends Base_MainTemplate
 {
-    public $template = 'welcomeTemplate';
+    //public $template = 'welcomeTemplate';
+    public $template = 'mainTemplate';
     
-    function before()
-    {
-        $ret = parent::before();
-
-        $this->session = Session::instance();
-        //$this->requireAuth();
-
-        $this->template->currentPage = "index";
-        $this->template->title = "index";
-        $this->template->account = new StdClass;
-        $this->template->account->displayName = $this->session->get('account_displayName');
-        $this->template->account->email = $this->session->get('account_email');
-        return $ret;
-    }
     public function action_index()
     {
         $this->template->content = "Hello! WORLD!";
