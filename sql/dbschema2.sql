7fc71e226d625c4581114fa1a8d06f85d9cef65a
Stephen T <stt@sfu.ca>
2011-04-04 21:40:16 -0700

    Lets not get field removal happy. Leave create/login fields in account.

--- sql/dbschema2.sql
+++ sql/dbschema2.sql
@@ -20,6 +20,8 @@ CREATE TABLE accounts (
    password CHAR(40) NOT NULL, -- You save one WHOLE byte by using CHAR instead of VARCHAR! lol.
    salt CHAR(10) NOT NULL, 		-- etc...
    status TINYINT NOT NULL 	  -- Status of account (unverified, verified, banned, etc)
+   created INT NOT NULL, -- Creation date
+   login INT -- Last login.
 ) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `conventions`;
