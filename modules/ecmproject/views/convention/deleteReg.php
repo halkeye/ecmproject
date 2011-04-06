704a8d1520d28aa7285b8146eaf0f86f6679d69f
Gavin <halkeye@gmail.com>
2011-04-06 01:23:18 -0700

    moving views

--- modules/ecmproject/views/convention/deleteReg.php
+++
@@ -1,8 +0,0 @@
-<p><strong>
-You are about to remove the registration for <?php echo $reg->gname . ' ' . $reg->sname ?> from your shoppping cart.</strong> 
-This action is <strong>NOT</strong> reversible.</p>
-<br />
-<?php echo form::open("convention/deleteReg/" . $reg->id); ?>
-<button name="No" value="No" type="submit" class='wide'>No</button>
-<button name="Yes" value="Yes" type="submit">Yes</button>
-<?php echo form::close(); ?>
--- modules/ecmproject/views/convention/paypalReturn.php
+++
@@ -1,3 +0,0 @@
-<div id="list">
- <p><strong>THANK YOU!</strong> You will get an email in your mailbox when payment is confirmed.</p>
-</div>
