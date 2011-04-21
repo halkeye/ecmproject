1bde38d4f9ae0d64111d2ab1367ecae73f4c4f9e
Stephen T <stt@sfu.ca>
2011-04-20 18:07:57 -0700

    Add support for first/last name fields for accounts.

--- application/views/admin/Account.php
+++ application/views/admin/Account.php
@@ -14,13 +14,20 @@
 	<p>An account is defined by an email address. Password must be typed in twice for verification.</p>		
 	<fieldset>		
 		<?php
-			foreach (array('email') as $field)
+			foreach (array('email', 'gname', 'sname', 'phone') as $field)
 			{
 				echo new View('global/_form_field', array('field'=>$field, 'fieldData'=>$fields[$field], 'value' => $row[$field], 'hasError'=>isset($errors[$field]) && $errors[$field]));
 			}
-			echo new View('global/_form_field', array('field'=>'password', 'fieldData'=>'text', 'value' => '', 'hasError'=>isset($errors[$field]) && $errors[$field]));			
-			echo new View('global/_form_field', array('field'=>'confirm_password', 'fieldData'=>'text', 'value' => '', 'hasError'=>isset($errors[$field]) && $errors[$field]));			
+					
 		?>
+	</fieldset>
+	<h1>Password</h1>
+	<p>Set the login password for this account.</p>		
+	<fieldset>
+			<?php 
+				echo new View('global/_form_field', array('field'=>'password', 'fieldData'=>'text', 'value' => '', 'hasError'=>isset($errors[$field]) && $errors[$field]));			
+				echo new View('global/_form_field', array('field'=>'confirm_password', 'fieldData'=>'text', 'value' => '', 'hasError'=>isset($errors[$field]) && $errors[$field]));	
+			?>
 	</fieldset>
 	<h1>Email Validation Status</h1>
 	<p>Admins may override the default email validation status. Setting status to VERIFIED or BANNED will prevent a validation email from going out to the email specified above.</p>		
