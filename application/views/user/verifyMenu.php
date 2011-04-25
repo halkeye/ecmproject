45f4b7f9ffa362f3b80c227b14730560343f84d0
Gavin <halkeye@gmail.com>
2011-04-25 10:50:52 -0700

    fix resend link links

--- application/views/user/verifyMenu.php
+++ application/views/user/verifyMenu.php
@@ -4,9 +4,9 @@
 <p class='fixed'>
     Please enter the verification code that was sent to your email address. 
     If the message is not in your mailbox, please check your junk mail or have it 
-    <strong><?php echo html::anchor('/user/resendVerification', 'resent to you') ?>.</strong>
+    <strong><?php echo html::anchor('/user/resendVerification', 'resent to you', NULL, NULL, TRUE) ?>.</strong>
      Alternatively, you can try 
-    <strong><?php echo html::anchor('/user/changeEmail', 'changing your email address') ?></strong>
+    <strong><?php echo html::anchor('/user/changeEmail', 'changing your email address', NULL, NULL, TRUE) ?></strong>
      if what you entered for your email was wrong.
 </p>
 <fieldset>
