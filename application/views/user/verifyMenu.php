9df350bd88fdf804e1c1270f4f0ba61f1289dd41
Stephen T <stt@sfu.ca>
2011-04-30 15:39:39 -0700

    Cosmetic fixes. This view wasn't fixed on the last style change.

--- application/views/user/verifyMenu.php
+++ application/views/user/verifyMenu.php
@@ -1,20 +1,21 @@
-<div id='form'> 
-<?php echo form::open('/user/validate'); ?>
-<h2 class="grey">Verify Email</h2>
-<p class='fixed'>
-    Please enter the verification code that was sent to your email address. 
+<h3>Verify Email</h3>
+<p>Please enter the verification code that was sent to your email address. 
     If the message is not in your mailbox, please check your junk mail or have it 
     <strong><?php echo html::anchor('/user/resendVerification', 'resent to you', NULL, NULL, TRUE) ?>.</strong>
      Alternatively, you can try 
     <strong><?php echo html::anchor('/user/changeEmail', 'changing your email address', NULL, NULL, TRUE) ?></strong>
      if what you entered for your email was wrong.
 </p>
+
+<?php echo form::open('/user/validate'); ?>
 <fieldset>
-    <label for="verifyCode">Verification Code <span class="small">The code in the email message...</span></label> 
-    <input type="text" id="verifyCode" name="verifyCode" value=""  /> 
+	<div>
+		<label for="verifyCode">Verification Code <span class="small">The code in the email message...</span></label> 
+		<input type="text" id="verifyCode" name="verifyCode" value=""  /> 
+	</div>
 </fieldset>
 <fieldset class='left'> 
-    <input type="submit" value="Validate"  />        
+    <input type="submit" value="Validate" class='submit' />        
 </fieldset>
 
 <?php echo form::close(); ?>
