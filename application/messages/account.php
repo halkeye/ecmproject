8b4f960b07156ef2ea3132f501c8cc5328014967
Stephen T <stt@sfu.ca>
2011-04-30 23:50:51 -0700

    Add validation messages for unique email/phone (account)

---
+++ application/messages/account.php
@@ -0,0 +1,12 @@
+<?php 
+ 
+return array( 
+	'email'		   => array(
+		'_valid_unique' 	=> __('This email address is already in use.'), 
+	),
+	'phone'		   => array(
+		'_valid_unique'		=> __('This phone number is already in use.'),
+	),
+); 
+
+?>
